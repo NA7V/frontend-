9246739b535b05887d4928db7dcf14645eaa65ef
NA7V <nenufartorrealba@gmail.com>
2022-10-11 11:00:43 -0300

    portafolio web fase 1

---
+++ example01.ts
@@ -0,0 +1,4 @@
+let a: number; //* inferencia explicita
+let b: string; //* inferencia explicita
+let c=101;     //* inferencia implicita
+ let recursos: string[] = ['memoria','disco','procesador'];
